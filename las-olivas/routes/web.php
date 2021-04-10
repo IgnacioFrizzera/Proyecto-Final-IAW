56e6ebe5e3203d1d6678420a5fbb45c9edcba170
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2021-04-10 09:33:36 -0300

    Modifico rutas y agrego middleware

--- las-olivas/routes/web.php
+++ las-olivas/routes/web.php
@@ -14,11 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/auth/login');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/clients-dashboard', 'App\Http\Controllers\ClientController@index')->name('clients-dashboard');
+});
 
 require __DIR__.'/auth.php';
