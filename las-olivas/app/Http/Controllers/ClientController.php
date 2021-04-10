2cf1e20b98f7185266ee2759d1dfd1bcd714da00
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2021-04-10 20:10:00 -0300

    Listado de clientes y modificaciones al navbar

--- las-olivas/app/Http/Controllers/ClientController.php
+++ las-olivas/app/Http/Controllers/ClientController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Client;
+
 
 class ClientController extends Controller
 {
     public function index()
     {
-        return view('clients-dashboard');
+        $clients = Client::all();
+
+        if(count($clients) == 0)
+        {
+            return view('clients-dashboard')->withMessage('No hay clientes cargados en el sistema');
+        }
+
+        return view('clients-dashboard')->withClients($clients);
     }
 }
